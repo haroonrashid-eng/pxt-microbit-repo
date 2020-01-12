c5360dcbf982ea4576a27d8f386a3da4161396a3
haroonrashid-eng <58487923+haroonrashid-eng@users.noreply.github.com>
2020-01-12 21:11:52 +0000

    Updates.

--- main.ts
+++ main.ts
@@ -1,7 +1,22 @@
-basic.showLeds(`
-    . . . . .
-    . # . # .
-    . . . . .
-    # . . . #
-    . # # # .
-    `);
+enum YearGroups {
+    Year3,
+    Year4,
+    Year5,
+    Year6
+}
+
+enum Classes {
+    Sapphire,
+    Topaz,
+    Obsidian,
+    Amber,
+    Emerald,
+    Diamond,
+    Ruby,
+    Coral
+}
+
+
+
+
+
